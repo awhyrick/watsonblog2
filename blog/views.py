73b3c6f7766af4d6fbe8a4c73a88f2d7612ea479
Amanda Whyrick <awhyrick@unomaha.edu>
2019-11-23 20:10:08 -0600

    First Commit

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,56 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+import json
+from ibm_watson import ToneAnalyzerV3
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('IvN00HN58QEdRm3IR4ArSeKcjX5Ov8bsJMyOV6ZXUBnU')
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    authenticator=authenticator
+)
+tone_analyzer.set_service_url('https://gateway.watsonplatform.net/tone-analyzer/api')
+
+authenticator = IAMAuthenticator('0qTkvVfjZFq5xnHR_VS4OFQmpJtbobgh5yUnPsCS99_3')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url('https://gateway.watsonplatform.net/language-translator/api')
+
+def tone(self, tone_input, content_type, sentences, tones, content_language, accept_language, param):
+    pass
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    for post in posts:
+        posting = post.text
+        translation = language_translator.translate(
+            text=post.text, model_id='en-es').get_result()
+        obj = (json.dumps(translation, indent=2, ensure_ascii=False))
+        print(obj)
+        obj2 = json.loads(obj)
+        post.obj2 = obj2['translations'][0]['translation']
+        post.w_count = obj2['word_count']
+        post.c_count = obj2['character_count']
+        text = post.text
+
+        tone_analysis = tone_analyzer.tone({'text': text}, context_type='application/json').get_result()
+        post.analysis = json.dumps(tone_analysis, indent=2)
+        post.doctone = json.loads(post.analysis)
+        post.tone = post.doctone["document_tone"]
+        the_tones = post.tone["tones"]
+        post.scores = the_tones
+
+    return render(request, 'blog/post_list.html', {'posts': posts},)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_new(request):
     if request.method == "POST":
@@ -29,4 +64,21 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
 
